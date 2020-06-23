39368cf4389d22e934806758bcbcbc072b9320fc
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-06-23 22:04:20 +0200

    creates basic api call

--- src/app/services/tox-service.service.ts
+++ src/app/services/tox-service.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToxServiceService {
+  baseUrl = 'http://localhost:3000/api'
+  constructor (private _http: HttpClient) {}
 
-  constructor() { }
+  ReadAllToxMol () {
+    return this._http.get(this.baseUrl + '/toxMolecules')
+  }
 }
