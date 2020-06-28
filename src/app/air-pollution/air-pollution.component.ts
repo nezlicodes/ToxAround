fb831304a1aed5a90d1e1f5cbb0a9403790a3e74
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-06-28 19:53:04 +0200

    WIP

--- src/app/air-pollution/air-pollution.component.ts
+++ src/app/air-pollution/air-pollution.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-air-pollution',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AirPollutionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('https://api.waqi.info/feed/algiers/?token=d0108c8944a13c3f95dd49fc872012c0d1a45843').subscribe((res:any) => console.log(res.data));
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router'
 import { ToxCreateComponent } from './tox-create/tox-create.component'
 import { ToxListComponent } from './tox-list/tox-list.component'
 import { ToxSingleComponent } from './tox-single/tox-single.component'
+import { AirPollutionComponent } from './air-pollution/air-pollution.component'
 
 const routes: Routes = [
   { path: 'create', component: ToxCreateComponent },
   { path: '', component: ToxListComponent },
-  { path: 'single', component: ToxSingleComponent }
+  { path: 'single', component: ToxSingleComponent },
+  {path: 'APD', component: AirPollutionComponent}
 ];
 
 
