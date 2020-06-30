037d28fec13493e61634d3f01a265c610711268d
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-06-30 08:47:09 +0200

    removes unnexessary data

--- src/app/air-pollution/air-pollution.component.ts
+++ src/app/air-pollution/air-pollution.component.ts
@@ -1,19 +1,16 @@
 import { Component, OnInit } from '@angular/core'
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { APIService } from '../services/api.service'
+
 @Component({
   selector: 'app-air-pollution',
   templateUrl: './air-pollution.component.html',
   styleUrls: ['./air-pollution.component.css']
 })
 export class AirPollutionComponent implements OnInit {
-  constructor (private http: HttpClient, private api: APIService) {}
+  constructor () {}
 
   ngOnInit () {
     
   }
 
-  getData () {
-
-  }
+ 
 }
