e8c7e0d69a1ba82155244b65daca96212071f8cc
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-06-30 08:52:07 +0200

    creates form

--- src/app/air-pollution/air-pollution.component.ts
+++ src/app/air-pollution/air-pollution.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-air-pollution',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./air-pollution.component.css']
 })
 export class AirPollutionComponent implements OnInit {
-  constructor () {}
+  constructor (private _fb:FormBuilder) {}
 
+  private userData:FormGroup
   ngOnInit () {
-    
+    this._fb.group({
+      lastname: [null, Validators.required],
+      firstname: [null, Validators.required],
+      sessionDate: [null, Validators.required],
+      email:[null, [Validators.required, Validators.email]]
+    });
   }
 
  
